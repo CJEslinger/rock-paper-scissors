c90d4a7fc575a287b217781ec4c1e61e55f46025
CJ Eslinger <cjeslinger28@gmail.com>
2022-05-25 11:13:09 -0400

    created functions and variables for game

--- app.js
+++ app.js
@@ -1,12 +1,52 @@
-const hand = ["Rock", "Paper", "Scissors"];
+const hands = ["rock", "paper", "scissors"];
+let computerSelection;
+let userSelection;
+computerSelection = computerPlay();
+userSelection = userPlay();
 
-function computerPlay() {
-    return hand[Math.floor(Math.random()*hand.length)];
+game();
+
+function game() {
+    playRound(computerSelection, userSelection);
+    console.log(userSelection);
+    console.log(computerSelection);
 }
 
-let computerSelection = computerPlay();
-playerSelection = prompt("Choose Rock, Paper, or Scissors")
+function computerPlay() {
+    return hands[Math.floor(Math.random()*hands.length)];
+}
 
-function playRound(computerSelection, playerSelection) {
-    
+function userPlay() {
+    let hand = prompt("Enter rock, paper, or scissors");
+    if (typeof hand === 'string') {
+        hand = hand.toLowerCase();
+        switch(hand) {
+            case 'rock':
+                return hand;
+            case 'paper':
+                return hand;
+            case 'scissors':
+                return hand;
+            default:
+                console.log("Invalid, type rock, paper or scissors.");
+                userPlay();
+        }
+    }
+}
+
+function playRound(user, computer) {
+    if (checkIfTie()) {
+        console.log("It's a tie!");
+    }
+
+}
+
+function checkIfTie() {
+    if (userSelection == computerSelection) {
+        return true;
+    }
+
+    else {
+        return false;
+    }
 }
